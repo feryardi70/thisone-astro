ac58c3f3f8dbe7dfa96f3e06a9ee05ac606f09ea
feryardi70 <fery.ardiansyah94747@gmail.com>
2025-03-15 20:16:12 +0700

    first commit

---
+++ src/pages/api/departure.json.ts
@@ -0,0 +1,18 @@
+import type { APIRoute } from 'astro';
+//import { getStore } from '@netlify/blobs';
+
+export const prerender = false;
+
+export const GET: APIRoute = async (context) => {
+    //const urlParams = new URL(context.url);
+    //const key = urlParams.searchParams.get('key');
+    const departures = [{"_id":"670d3779e22aa936c07553e8","airline":"IW","flightnumber":1265,"destination":"Medan","departdate":"2024-10-15","departtime":"06:15","gate":"01","remark":"Delay","__v":0}]
+    
+    return new Response(
+        JSON.stringify({
+          msg: 'success',
+          data: departures
+        }), {status: 200}
+      );
+    
+};
