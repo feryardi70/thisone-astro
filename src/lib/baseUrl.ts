7ec8e25da23b39d43b1f0db80bde577347a22cae
feryardi70 <fery.ardiansyah94747@gmail.com>
2025-03-23 00:45:31 +0700

    forgot edit url

--- src/lib/baseUrl.ts
+++ src/lib/baseUrl.ts
@@ -1,5 +1,5 @@
-export const baseUrl = "http://localhost:4321";
-// export const baseUrl = "https://thisone-astro.netlify.app";
+// export const baseUrl = "http://localhost:4321";
+export const baseUrl = "https://thisone-astro.netlify.app";
 
-export const APIUrl = "http://localhost";
-// export const APIUrl = "https://thisone.my.id";
+// export const APIUrl = "http://localhost";
+export const APIUrl = "https://thisone.my.id";
